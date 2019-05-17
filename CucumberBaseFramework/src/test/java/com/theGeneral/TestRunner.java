a7fb4ce399c94680b94641a7051635af45c4adb9
sivahtcglobal <siva.htcglobal@gmail.com>
2019-05-17 16:55:21 +0530

    tag update
    tag update

--- CucumberBaseFramework/src/test/java/com/theGeneral/TestRunner.java
+++ CucumberBaseFramework/src/test/java/com/theGeneral/TestRunner.java
@@ -18,7 +18,7 @@ import cucumber.api.testng.TestNGCucumberRunner;
 
 
 @CucumberOptions(features = "src/test/resources/features",monochrome=false,strict=true, glue = { "com.theGeneral.stepDefinations" }, tags = {
-				"@Login" }, plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
+				"@NewQuote" }, plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
 						"json:target/cucumber-reports/CucumberTestReport.json",
 						"rerun:target/cucumber-reports/rerun.txt" })
 public class TestRunner extends BaseClass {
